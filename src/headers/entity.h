903447ae633966919339b83e1ed905dafbcb834c
norbcodes <thenorbert2008@gmail.com>
2023-05-21 10:47:01 +0200

    A bunch of code

---
+++ src/headers/entity.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "node.h"
+
+/*
+This file contains the Enemy class and functions for entity handling from entity.cpp.
+*/
+
+struct Enemy
+{
+    char x;
+    char y;
+    float speed;
+    int health;
+    char weakness;  // 0 = Fire, 1 = Electricity, 2 = Laser
+
+    Enemy(char type, Node* start_node)
+    {
+        /*
+        Still gotta write out all the enemy types...
+        */
+    }
+};
---
+++ src/headers/node.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/*
+This file contains the AI Node class and functions from node.cpp.
+*/
+
+struct Node
+{
+    char x;
+    char y;
+    Node* link;
+
+    Node(char X, char Y, Node* next_node)
+        :x(X), y(Y)
+    {
+        this->link = next_node;
+    }
+
+};
